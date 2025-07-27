49a5c7a6400bc39f98c067057a5381c43be31631
Mabian <Taskan.Barnak@gmail.com>
2025-07-27 17:47:02 +0200

    Create svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const textChoices: string[] = ['Hello Wööööörld!', 'Hello Hell!'];
+	let textToShow: string = textChoices[0];
+	function doMagic() {
+		textToShow = textToShow === textChoices[0] ? textChoices[1] : textChoices[0];
+	}
+</script>
+
+<div class="flex flex-col items-center">
+	<button onclick="{doMagic}">Do Magic</button>
+	<div class="mt-1">{textToShow}</div>
+</div>
